ed90e5bad3949b123e1ed7d45d27401190e6efb0
Mathias Gaunard <mathias@gaunard.com>
2012-09-25 00:26:24 +0200

    make the operator overloads use is_equal etc. rather than compare_equal. Move files across Boost.SIMD modules and toolboxes accordingly

---
+++ modules/boost/simd/operator/include/boost/simd/toolbox/operator/functions/simd/sse/sse2/is_greater.hpp
@@ -0,0 +1,133 @@
+//==============================================================================
+//         Copyright 2003 - 2011 LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011 LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_SSE_SSE2_IS_GREATER_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_OPERATOR_FUNCTIONS_SIMD_SSE_SSE2_IS_GREATER_HPP_INCLUDED
+#ifdef BOOST_SIMD_HAS_SSE2_SUPPORT
+#include <boost/simd/toolbox/operator/functions/is_greater.hpp>
+#include <boost/simd/include/functions/simd/is_equal.hpp>
+#include <boost/simd/include/functions/simd/logical_or.hpp>
+#include <boost/simd/include/functions/simd/logical_and.hpp>
+#include <boost/simd/sdk/simd/logical.hpp>
+#include <boost/dispatch/meta/downgrade.hpp>
+#include <boost/dispatch/meta/as_integer.hpp>
+
+namespace boost { namespace simd { namespace ext
+{
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_greater_, boost::simd::tag::sse2_
+                            , (A0)
+                            , ((simd_<double_<A0>,boost::simd::tag::sse_>))
+                              ((simd_<double_<A0>,boost::simd::tag::sse_>))
+                            )
+  {
+    typedef typename meta::as_logical<A0>::type result_type;
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+      return _mm_cmpgt_pd(a0,a1);
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_greater_, boost::simd::tag::sse2_
+                            , (A0)
+                            , ((simd_<single_<A0>,boost::simd::tag::sse_>))
+                              ((simd_<single_<A0>,boost::simd::tag::sse_>))
+                            )
+  {
+    typedef typename meta::as_logical<A0>::type result_type;
+
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+      return _mm_cmpgt_ps(a0,a1);
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_greater_, boost::simd::tag::sse2_
+                            , (A0)
+                            , ((simd_<unsigned_<A0>,boost::simd::tag::sse_>))
+                              ((simd_<unsigned_<A0>,boost::simd::tag::sse_>))
+                            )
+  {
+    typedef typename meta::as_logical<A0>::type result_type;
+
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+      typedef typename dispatch::meta::as_integer<A0, signed>::type stype;
+      return  bitwise_cast<result_type>
+              ( boost::simd::gt ( bitwise_cast<stype>(a0) - Signmask<stype>()
+                        , bitwise_cast<stype>(a1) - Signmask<stype>()
+                        )
+              );
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_greater_, boost::simd::tag::sse2_
+                            , (A0)
+                            , ((simd_<int8_<A0>,boost::simd::tag::sse_>))
+                              ((simd_<int8_<A0>,boost::simd::tag::sse_>))
+                            )
+  {
+    typedef typename meta::as_logical<A0>::type result_type;
+
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+      return _mm_cmpgt_epi8(a0,a1);
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_greater_, boost::simd::tag::sse2_
+                            , (A0)
+                            , ((simd_<int16_<A0>,boost::simd::tag::sse_>))
+                              ((simd_<int16_<A0>,boost::simd::tag::sse_>))
+                            )
+  {
+    typedef typename meta::as_logical<A0>::type result_type;
+
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+      return _mm_cmpgt_epi16(a0,a1);
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_greater_, boost::simd::tag::sse2_
+                            , (A0)
+                            , ((simd_<int32_<A0>,boost::simd::tag::sse_>))
+                              ((simd_<int32_<A0>,boost::simd::tag::sse_>))
+                            )
+  {
+    typedef typename meta::as_logical<A0>::type result_type;
+
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+      return _mm_cmpgt_epi32(a0,a1);
+    }
+  };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::is_greater_, boost::simd::tag::sse2_
+                            , (A0)
+                            , ((simd_<int64_<A0>,boost::simd::tag::sse_>))
+                              ((simd_<int64_<A0>,boost::simd::tag::sse_>))
+                            )
+  {
+    typedef typename meta::as_logical<A0>::type result_type;
+
+    BOOST_SIMD_FUNCTOR_CALL_REPEAT(2)
+    {
+      typedef typename dispatch::meta::downgrade<A0, signed>::type type;
+      typedef typename dispatch::meta::downgrade<A0, unsigned>::type utype;
+      utype al = _mm_shuffle_epi32(a0(), _MM_SHUFFLE(2, 2, 0, 0));
+      utype bl = _mm_shuffle_epi32(a1(), _MM_SHUFFLE(2, 2, 0, 0));
+      type ah  = _mm_shuffle_epi32(a0(), _MM_SHUFFLE(3, 3, 1, 1));
+      type bh  = _mm_shuffle_epi32(a1(), _MM_SHUFFLE(3, 3, 1, 1));
+
+      return bitwise_cast<result_type>( l_or(boost::simd::gt(ah,bh), l_and(boost::simd::eq(ah,bh), boost::simd::gt(al,bl))) );
+    }
+  };
+} } }
+
+#endif
+#endif
